a0968a33b1ceca4b636c876c1fcbcf47bf7af7b2
Gaurav Tarlok Kakkar <gkakkar7@simontam.cc.gatech.edu>
2020-11-22 13:04:58 -0500

    object store size increased

--- srtml_exp/scripts/model_repository.py
+++ srtml_exp/scripts/model_repository.py
@@ -26,10 +26,15 @@ def plsmr(s3_url, endpoint):
 @click.command()
 def start_srtml():
 
+    # os.system(
+    #     'ray start --head --resources \'{"Titan V": 8, "Tesla P40": 8, "Radeon '
+    #     'RX 5000": 12, "Radeon RX Vega 64": 12, "CPU1": 8, "CPU2": 8, "FPGA1": '
+    #     '4, "FPGA2": 4}\' --object-store-memory 1000000000 --num-cpus 24 '
+    #     '--internal-config \'{"max_direct_call_object_size": 10485760, '
+    #     '"max_grpc_message_size": 104857600}\''
+    # )
     os.system(
         'ray start --head --resources \'{"Titan V": 8, "Tesla P40": 8, "Radeon '
         'RX 5000": 12, "Radeon RX Vega 64": 12, "CPU1": 8, "CPU2": 8, "FPGA1": '
-        '4, "FPGA2": 4}\' --object-store-memory 100000000 --num-cpus 24 '
-        '--internal-config \'{"max_direct_call_object_size": 10485760, '
-        '"max_grpc_message_size": 104857600}\''
+        '4, "FPGA2": 4}\' --object-store-memory 100000000000 --num-cpus 24 '
     )
